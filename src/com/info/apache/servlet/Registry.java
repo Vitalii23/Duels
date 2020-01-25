9f0b85919a7a42f47d13acd64e0e45894f116e74
Vitalii <danilov.vit.an@gmai.com>
2020-01-25 20:58:16 +0700

    prototype duel game

--- src/com/info/apache/servlet/Registry.java
+++ src/com/info/apache/servlet/Registry.java
@@ -14,17 +14,18 @@ import java.io.PrintWriter;
 
 public class Registry extends HttpServlet {
 
+    private AccountService accountService;
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html; charset=UTF-8");
         PrintWriter writer = resp.getWriter();
-        AccountServiceImpl accountService = new AccountServiceImpl();
+       // AccountServiceImpl accountService = new AccountServiceImpl();
 
         String login = req.getParameter("login");
         String password = req.getParameter("password");
 
         if (req.getParameter("add") != null){
-
             accountService.add(login, password);
             RequestDispatcher dispatcher = req.getRequestDispatcher("/registry.jsp");
             dispatcher.forward(req, resp);
